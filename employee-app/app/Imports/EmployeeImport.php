c6f4c0d012818ebcb707d2ec4911ce853e030caf
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-11-07 01:29:18 -0300

    Add company validation on employee import

--- employee-app/app/Imports/EmployeeImport.php
+++ employee-app/app/Imports/EmployeeImport.php
@@ -3,12 +3,14 @@
 namespace App\Imports;
 
 use App\Models\Employee;
+use Cache;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\SkipsErrors;
 use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
+use Symfony\Component\HttpFoundation\Response;
 
 class EmployeeImport implements SkipsOnError,ToModel,WithHeadingRow,WithUpserts
 {
@@ -40,9 +42,30 @@ class EmployeeImport implements SkipsOnError,ToModel,WithHeadingRow,WithUpserts
             'name' => $row['name'],
             'birth_date' => $row['birth_date'],
             'registration_number' => $row['registration_number'],
-            'company_id' => $row['company_id'],
+            'company_id' => $this->getEmployeeCompany($row['company_id']),
             'imported' => true,
             'import_file' => $this->fileName
         ]);
+    }
+
+    private function getEmployeeCompany(int $companyId)
+    {
+        $expiration = 60 * 24; // one day
+        $key = 'company_' . $companyId;
+
+        $companiesEndpoint = 'http://host.docker.internal:8003/api/companies/'.$companyId;
+        
+        $response = Cache::remember($key, $expiration, function() use($companiesEndpoint) {
+            return \Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ])->get($companiesEndpoint)->json();
+        });
+
+        if( array_key_exists('exception', $response) ) {
+            return null;
+        }
+
+        return $response[0]['id'];
     }
 }
