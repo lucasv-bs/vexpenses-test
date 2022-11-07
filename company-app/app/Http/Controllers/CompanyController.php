72f2575d61700335938874eb3ee37ac02c385185
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-11-07 00:19:30 -0300

    Add the CRUD for the Company model in the company-app

---
+++ company-app/app/Http/Controllers/CompanyController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Company::paginate(100));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'corporate_name' => 'required',
+            'cnpj' => 'required'
+        ]);
+
+        $company = Company::create($request->all());
+
+        return response()->json($company, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Company $company)
+    {
+        return response()->json(Company::find($company));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Company $company)
+    {
+        $data = $request->validate([
+            'corporate_name' => 'required',
+            'cnpj' => 'required'
+        ]);
+
+        $company->update($data);
+
+        return response()->json($company, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Company $company)
+    {
+        $company->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ company-app/tests/Feature/app/Http/Controllers/CompanyControllerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class CompanyControllerTest extends TestCase
+{
+    /**
+     * Feature test of get all companies.
+     *
+     * @return void
+     */
+    public function testAllCompaniesCanBeListed()
+    {
+        $response = $this->get(route('companies.index'));
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJson(
+            fn (AssertableJson $json) => $json->has('current_page')->etc()
+        );
+    }
+
+    /**
+     * Feature test of create an employee.
+     *
+     * @return void
+     */
+    public function testCompanyCanBeCreated()
+    {
+        $payload = [
+            'corporate_name' => fake()->company(),
+            'cnpj' => fake()->unique()->randomNumber(9)
+        ];
+
+        $response = $this->post(route('companies.store'), $payload);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertDatabaseHas('companies', $payload);
+    }
+
+    /**
+     * Test to validate submission of all required data for creating an employee.
+     *
+     * @return void
+     */
+    public function testAllMandatoryDataMustBeSubmittedToCreateAnCompany()
+    {
+        $payload = [
+            'foo' => 'bar'
+        ];
+
+        $response = $this->post(route('companies.store'), $payload, [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * Feature test of get an specific employee.
+     *
+     * @return void
+     */
+    public function testSpecificCompanyCanBeRetrieved()
+    {
+        $company = Company::factory()->create();
+
+        $response = $this->get(route('companies.show', $company->id));
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment([
+            "corporate_name" => $company->corporate_name,
+            "cnpj" => $company->cnpj
+        ]);
+    }
+
+    /**
+     * Feature test to update an employee.
+     *
+     * @return void
+     */
+    public function testCompanyCanBeUpdated()
+    {
+        $company = Company::factory()->create();
+
+        $company->corporate_name = "Company name updated";
+        $company = $company->toArray();
+
+        $response = $this->put(route('companies.update', $company['id']), $company);
+
+        $response->assertStatus(Response::HTTP_ACCEPTED);
+        $this->assertDatabaseHas('companies', $company);
+    }
+
+    /**
+     * Feature test to delete an employee.
+     *
+     * @return void
+     */
+    public function testCompanyCanBeDeleted()
+    {
+        $company = Company::factory()->create();
+
+        $response = $this->delete(route('companies.destroy', $company->id));
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+        $this->assertDatabaseMissing('companies', ['id' => $company->id]);
+    }
+}
